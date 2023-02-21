bb5adffd887d97d018fa9686e5c9376ecd2123b8
macbobby913 <mac4sm@gmail.com>
2023-02-21 11:28:33 +0800

    feat: 🎸 Added Footer anchor function
    All four buttons in the Footer component now anchors to the correct
    section when clicked

--- src/App.tsx
+++ src/App.tsx
@@ -11,15 +11,23 @@ function App() {
   const homeRef = useRef<HTMLDivElement>(null);
   const productRef = useRef<HTMLDivElement>(null);
   const specialRef = useRef<HTMLDivElement>(null);
+  const contactRef = useRef<HTMLDivElement>(null);
 
   const goToSection = (sectionAnchor: HTMLDivElement | null)=>{
     sectionAnchor?.scrollIntoView({behavior: "smooth"})
   };
   return (
     <div id={'mainContainer'}>
-      <HomeSection ref={homeRef}  goToContactUs={()=>{goToSection(specialRef.current)}}/>
-      <ProductSection ref={productRef} goToContactUs={()=>{goToSection(specialRef.current)}}/>
+      <HomeSection ref={homeRef}  goToContactUs={()=>{goToSection(contactRef.current)}}/>
+      <ProductSection ref={productRef} goToContactUs={()=>{goToSection(contactRef.current)}}/>
       <SpecialSection ref={specialRef}/>
+      <Contact ref={contactRef}/>
+      <Footer 
+        goToHome={()=>{goToSection(homeRef.current)}}
+        goToProduct={()=>{goToSection(productRef.current)}}
+        goToSpecial={()=>{goToSection(specialRef.current)}}
+        goToContactUs={()=>{goToSection(contactRef.current)}}
+        />
     </div>
   );
 }
